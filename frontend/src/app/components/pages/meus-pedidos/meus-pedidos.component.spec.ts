7eb75b32be8430463f72949a41504c9fccd4cc38
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-05 20:06:25 -0300

    feat: pagina meus pedidos.

---
+++ frontend/src/app/components/pages/meus-pedidos/meus-pedidos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MeusPedidosComponent } from './meus-pedidos.component';
+
+describe('MeusPedidosComponent', () => {
+  let component: MeusPedidosComponent;
+  let fixture: ComponentFixture<MeusPedidosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MeusPedidosComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MeusPedidosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
