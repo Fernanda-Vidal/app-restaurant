3b82f5cf831fe8a8bc6fe2c7eaada946998f0eec
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-06 20:30:13 -0300

    feat: lógica da tela login e salva no db.

--- frontend/src/app/components/pages/login/login.component.ts
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { appendFile } from 'fs';
+import { CustomerService } from 'src/app/services/customer/customer.service';
+import { ICustomer } from '../../../../../../backend/src/interfaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  router: Router;
+  name: string = '';
+  cpf: string = '';
+  table: number = 1;
+
+  constructor(
+    private customerService: CustomerService,
+    router: Router
+    ) { this.router = router;}
+
+  ngOnInit(): void {}
+
+ async handleClick()  {
+  const newCustomer: ICustomer = {
+    name: '',
+    cpf: '',
+    table: 0
+  };
+
+  newCustomer.name = this.name;
+  newCustomer.cpf = this.cpf;
+  newCustomer.table = this.table;
+
+  await this.customerService.createCustomer(newCustomer).subscribe();
+
+  this.name = '';
+  this.cpf = '';
+
+  this.router.navigate(['/cardapio'])
+ }
 }
