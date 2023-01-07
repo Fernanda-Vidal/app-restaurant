d5268c6207cc4e3730df1fa63e7a716308751af0
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-07 16:04:33 -0300

    feat: Inicia estilo do cardápio.

--- frontend/src/app/components/pages/cardapio/cardapio.component.ts
+++ frontend/src/app/components/pages/cardapio/cardapio.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DishService } from 'src/app/services/dishes/dish.service';
+
+import { IDish } from '../../../../../../backend/src/interfaces';
 
 @Component({
   selector: 'app-cardapio',
   templateUrl: './cardapio.component.html',
   styleUrls: ['./cardapio.component.css']
 })
-export class CardapioComponent {
+export class CardapioComponent implements OnInit {
+  dish?: IDish;
+
+  constructor(private dishesService: DishService) {}
+  async ngOnInit() {
+    const aaa = await this.dishesService.getAllDishes().subscribe()
+    
+    console.log(aaa)
+  }
+
 
 }
