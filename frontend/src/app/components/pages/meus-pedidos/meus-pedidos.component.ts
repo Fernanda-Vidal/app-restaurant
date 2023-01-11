930910560ab9136fe0841c5d7fd4cbe960a654df
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-11 12:19:23 -0300

    feat: alterações na pagina meus pedidos.

--- frontend/src/app/components/pages/meus-pedidos/meus-pedidos.component.ts
+++ frontend/src/app/components/pages/meus-pedidos/meus-pedidos.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/orders/order.service';
+
+import { IOrder } from '../../../../../../backend/src/interfaces';
 
 @Component({
   selector: 'app-meus-pedidos',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./meus-pedidos.component.css']
 })
 export class MeusPedidosComponent {
+  id: number = 2;
+  orders: IOrder[] = [];
 
+  constructor(private orderService: OrderService) {
+    this.orderService.getOrderByCustomer(this.id).subscribe((items) => {
+      this.orders = items;
+      console.log(this.orders)
+    })
+  }
 }
